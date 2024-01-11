47b689ff0e2b0ca421083c0a2cf6a5f3b05522fa
Jeevan Ghimire <imjeevanghimire@gmail.com>
2024-01-11 22:09:47 +0545

    minor fixes

--- script.js
+++ script.js
@@ -6,23 +6,20 @@ document.getElementById('thumbnailForm').addEventListener('submit', function (e)
     if (videoId) {
         const thumbnailUrl = `https://img.youtube.com/vi/${videoId}/maxresdefault.jpg`;
         document.getElementById('thumbnailContainer').innerHTML = `<img src="${thumbnailUrl}" alt="Thumbnail" width="600"
-        height="426">`;
-        downloadThumbnail();
+        height="420">`;
+        document.getElementById('downloadButton').setAttribute('onclick', `window.open('${thumbnailUrl}')`);
     } else {
         alert('Invalid YouTube URL. Please enter a valid URL.');
     }
 });
 
-function downloadThumbnail() {
-    const youtubeUrl = document.getElementById('youtubeUrl').value;
-    const videoId = getYoutubeVideoId(youtubeUrl);
-
-    if (videoId) {
-        const thumbnailUrl = `https://img.youtube.com/vi/${videoId}/maxresdefault.jpg`;
-        window.open(thumbnailUrl);
-    } else {
-        alert('Invalid YouTube URL. Please enter a valid URL.');
-    }
+window.onload = function() {
+    const downloadButton = document.createElement('button');
+    downloadButton.innerHTML = 'Download';
+    downloadButton.setAttribute('id', 'downloadButton');
+    downloadButton.setAttribute('class', 'btn btn-primary');
+    downloadButton.setAttribute('style', 'margin-top: 10px;');
+    document.getElementById('thumbnailContainer').appendChild(downloadButton);
 }
 
 function getYoutubeVideoId(url) {
