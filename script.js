79b86f879c6fdb8fdbe2ffb1bb360f4b4571f05b
Jeevan Ghimire <imjeevanghimire@gmail.com>
2024-01-11 21:46:57 +0545

    changes

--- script.js
+++ script.js
@@ -1,29 +1,27 @@
 document.getElementById('thumbnailForm').addEventListener('submit', function (e) {
     e.preventDefault();
     const youtubeUrl = document.getElementById('youtubeUrl').value;
+    const videoId = getYoutubeVideoId(youtubeUrl);
 
-        if (videoId) {
-            const thumbnailUrl = `https://img.youtube.com/vi/${videoId}/maxresdefault.jpg`;
-            document.getElementById('thumbnailContainer').innerHTML = `<img src="${thumbnailUrl}" alt="Thumbnail">`;
-            downloadthumbail(thumbnailUrl);
-        } else {
-            alert('Invalid YouTube URL. Please enter a valid URL.');
+    if (videoId) {
+        const thumbnailUrl = `https://img.youtube.com/vi/${videoId}/maxresdefault.jpg`;
+        document.getElementById('thumbnailContainer').innerHTML = `<img src="${thumbnailUrl}" alt="Thumbnail" width="640"
+        height="426">`;
+    } else {
+        alert('Invalid YouTube URL. Please enter a valid URL.');
     }
 });
 
+function downloadThumbnail() {
+    const youtubeUrl = document.getElementById('youtubeUrl').value;
+    const videoId = getYoutubeVideoId(youtubeUrl);
 
-function downloadthumbail(url){
-    var link = document.createElement('a');
-    link.href = url;
-    link.download = 'thumbnail.jpg';
-    document.body.appendChild(link);
-    link.click();
-    document.body.removeChild(link);
-
-}
-function isValidYoutubeUrl(url) {
-    const regExp = /^(https?:\/\/)?(www\.)?youtube\.com\/watch\?v=[\w-]{11}$/;
-    return regExp.test(url);
+    if (videoId) {
+        const thumbnailUrl = `https://img.youtube.com/vi/${videoId}/maxresdefault.jpg`;
+        window.open(thumbnailUrl);
+    } else {
+        alert('Invalid YouTube URL. Please enter a valid URL.');
+    }
 }
 
 function getYoutubeVideoId(url) {
